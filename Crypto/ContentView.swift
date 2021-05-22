5b0fb407854a1cadc024239b0361fbc52dda8998
Anushkinz <anuarprogramm@gmail.com>
2021-05-22 22:23:45 +0600

    Crypro

--- Crypto/ContentView.swift
+++ Crypto/ContentView.swift
@@ -6,16 +6,78 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    
+    @ObservedObject private var viewModel = CoinListViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(viewModel.coinViewModels, id: \.self){
+                CoinViewModel in
+                Image(CoinViewModel.iconURL)
+                Text(CoinViewModel.displayText)
+            }.onAppear{
+                self.viewModel.fetchCoins()
+            }.navigationBarTitle("Coins")
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+class CoinListViewModel: ObservableObject{
+    
+    private let cryptoService = CryptoSrvice()
+    
+    @Published var coinViewModels = [CoinViewModel]()
+    
+    var cancellable: AnyCancellable?
+    
+    func fetchCoins()  {
+        cancellable = cryptoService.fetchCoins().sink(receiveCompletion: { _ in
+            
+        }, receiveValue: { cryptocontainer in
+            self.coinViewModels = cryptocontainer.data.coins.map {CoinViewModel($0)}
+            print(self.coinViewModels)
+        })
+    }
+}
+
+struct CoinViewModel: Hashable {
+    private let coin: Coin
+    
+    var name: String{
+        return coin.name
+    }
+    
+    var formattedPrice: String{
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        
+        guard let price = Double(coin.price), let formattedPrice = numberFormatter.string(from: NSNumber(value: price)) else {
+            return ""
+        }
+        
+        return formattedPrice
+    }
+    
+    var iconURL: String{
+        return coin.iconURL
+    }
+    
+    var displayText: String{
+        return name + " - " + formattedPrice
+    }
+    
+    init(_ coin: Coin) {
+        self.coin = coin
     }
 }
