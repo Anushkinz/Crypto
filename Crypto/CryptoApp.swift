4d67ff1fbbe9fa8c8489c8033f6e426fe37a7e23
Anushkinz <anuarprogramm@gmail.com>
2021-05-22 21:19:50 +0600

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by admin on 22/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
